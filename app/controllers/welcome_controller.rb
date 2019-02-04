5ba16a0bae2dbc179a559cd641284a53e2dd3fc9
lebonvieuxlion <debaugelion@gmail.com>
2019-02-04 17:11:49 +0100

    Création page welcome avec tous les potins et adaptation nav en conséquence

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,13 @@
 class WelcomeController < ApplicationController
   
   def index_welcome	
-  	@first_name = params[:first_name]
-  	# @gossip_title = Gossip.all.length.times do |g, i|
-  	#	g.find(i).title
-  	@gossip_title = Gossip.find(params[:title])
+  	@firstname = params[:first_name]
+
+  	@gossips = Gossip.all
+
+
   	end
   	
-  end
+  
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   
   get 'contact', to: 'contact#index_contact'
   get 'team', to: 'team#index_team'
-  get 'welcome/:first_name', to: 'welcome#index_welcome'
+  get 'welcome/:first_name', to: 'welcome#index_welcome', as: 'welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
