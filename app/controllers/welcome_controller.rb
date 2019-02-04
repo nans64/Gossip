4e35bda63c2800a80989953139a58cc50b99af9d
lebonvieuxlion <debaugelion@gmail.com>
2019-02-04 18:19:30 +0100

    Création page auteur lié à la page potin

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,6 +9,11 @@ class WelcomeController < ApplicationController
  	def index_link_to_gossip
  		@id_gossip = params[:gossip_id]
  	end
+
+ 	def index_link_to_author
+ 		@author_id = params[:author_id]
+
+ 	end
   
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'team', to: 'team#index_team'
   get 'welcome/:first_name', to: 'welcome#index_welcome', as: 'welcome'
   get 'gossip/:gossip_id', to: 'welcome#index_link_to_gossip', as: 'gossip'
+  get 'author/:author_id', to: 'welcome#index_link_to_author', as: 'author'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
