8762a0adf97134151070bcc57853e688a1c601be
lebonvieuxlion <debaugelion@gmail.com>
2019-02-05 15:18:30 +0100

    début test models et validations

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -2,4 +2,7 @@ class Gossip < ApplicationRecord
 	belongs_to :user
 	has_many :posts
 	has_many :tags, through: :posts
+
+	validates :title, length: {minimum: 3, maximum: 14}
+	validates :content, presence: true
 end
